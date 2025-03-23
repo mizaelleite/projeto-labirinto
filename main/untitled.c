5d2a1ef1f3609e7abc8e254d1ca5bc31f171980c
flaviocwz <flavio.cruz@dcomp.ufs.br>
2025-03-23 15:05:54 -0300

    Add files via upload

--- main/untitled.c
+++ main/untitled.c
@@ -1,82 +1,97 @@
 #include <stdio.h>
 #include <string.h>
 
+
 typedef struct {
-	char simbolo;
-	int visitada; 
-	
-	} celula;
-	
-int main () {
+    char simbolo;
+    int visitada; 
+} celula;
 
-	int row, column;
-	char input[15], input2[15];
-	char t[] = ".txt"; // criei uma nova string só para o usuário não precisar colocar o .txt e abrir direto
-	
-	printf("Digite o nome do arquivo que deseja abrir: ");
-	
-	fgets(input, sizeof(input), stdin); // pegar o input do usuário e armazenar em input
-	
-	input[strcspn(input, "\n")] = '\0'; // substituir a quebra de linha pelo elemento de quebra de uma string.
-	
-	strcat(input, t); // concatenar o nome do arquivo com .txt
-	printf("\n"); // adicionar uma quebra de linha adicional para ficar mais bonitinho
 
-	FILE *arquivo = fopen(input, "r");
-	  if(arquivo == NULL) {
-    		printf("Falha ao acessar o arquivo\n"); // Caso o usuário coloque um arquivo inexistente
-    		return 1;
-  	}
-	fscanf(arquivo, "%d %d", &row, &column);
-	
-	celula labirinto[row][column];
+int main(int argc, char *argv[]) {
+    // Verifica se o usuário passou o nome do arquivo de entrada
+    if (argc < 2) {
+        printf("Uso: %s <arquivo_de_entrada>\n", argv[0]);
+        return 1;
+    }
 
-	for (int i = 0; i < row; i++) { 
-		for (int j = 0; j < column; j++) {                     // laço para pegar os elementos do arquivo e armazenar em uma matriz
-			
-			fscanf(arquivo, " %c", &labirinto[i][j].simbolo);
-			labirinto[i][j].visitada = 0;
-			}
-		}
-		
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < column; j++) {                     // laço para imprimir a matriz já formatada na tela
-			printf("%c ", labirinto[i][j].simbolo);
-			}
-			printf("\n");
-		}
-	printf("\n");
-	
-	printf("Digite como deseja salvar o nome do arquivo: ");
-	
-	fgets(input2, sizeof(input2), stdin); // pegar o input do usuário e armazenar em input
-	
-	input2[strcspn(input2, "\n")] = '\0'; // substituir a quebra de linha pelo elemento de quebra de uma string.
-	
-	strcat(input2, t); // concatenar o nome do arquivo com .txt
- 
-	arquivo = fopen(input2, "w"); 
-	 
-	 for (int i = 0; i < row; i++){ // laço for para printar os elementos do labirinto em outro arquivo e deixar salvo.
-		 
-		 for (int j = 0; j < column; j++) {
-			 
-				fprintf(arquivo, "%c ", labirinto[i][j].simbolo);
-			 }
-		 
-		 fprintf(arquivo, "\n");
-		 }
-		
-	 printf("\n");
-	 
-	 printf("SALVO COM SUCESSO!\n");
-		 
-		 
-	 fclose(arquivo);
-	 
 
-	
-	
-return 0;
-	
+    // Abre o arquivo de entrada para leitura
+    FILE *arquivo = fopen(argv[1], "r");
+    if (!arquivo) {
+        printf("Erro ao abrir o arquivo %s\n", argv[1]);
+        return 1;
+    }
+
+
+    // Lê as dimensões do labirinto
+    int row, column;
+    fscanf(arquivo, "%d %d", &row, &column);
+    
+    // Declara a matriz do labirinto
+    celula labirinto[row][column];
+
+
+    // Lê os dados do arquivo e armazena na matriz
+    for (int i = 0; i < row; i++) { 
+        for (int j = 0; j < column; j++) {
+            fscanf(arquivo, " %c", &labirinto[i][j].simbolo);
+            labirinto[i][j].visitada = 0;
+        }
+    }
+    
+    // Fecha o arquivo de entrada após a leitura
+    fclose(arquivo);
+
+
+    // Exibe o labirinto carregado na tela
+    printf("\nLabirinto carregado:\n");
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            printf("%c ", labirinto[i][j].simbolo);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+
+    // Pede ao usuário o nome do arquivo de saída
+    char input2[15];
+    char t[] = ".txt"; 
+
+
+    printf("Digite o nome do arquivo para salvar: ");
+    fgets(input2, sizeof(input2), stdin);
+
+
+    // Remove a quebra de linha do nome digitado
+    input2[strcspn(input2, "\n")] = '\0';
+    
+    // Adiciona a extensão .txt automaticamente
+    strcat(input2, t);
+
+
+    // Abre o arquivo para escrita
+    arquivo = fopen(input2, "w"); 
+    if (!arquivo) {
+        printf("Erro ao criar o arquivo de saída\n");
+        return 1;
+    }
+
+
+    // Escreve o labirinto no arquivo de saída
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            fprintf(arquivo, "%c ", labirinto[i][j].simbolo);
+        }
+        fprintf(arquivo, "\n");
+    }
+
+
+    // Fecha o arquivo de saída
+    fclose(arquivo);
+    
+    printf("\nSALVO COM SUCESSO em '%s'!\n", input2);
+    
+    return 0;
 }
